324dc1ad0067be4c507dfc537de120ec182cbf83
Invisible <fahimaust609@gmail.com>
2022-06-07 23:47:53 +0600

    Mk Tech

---
+++ index.js
@@ -0,0 +1,92 @@
+function mymessage1(){
+    let myvar1=document.querySelector("#htitle")
+    let myvar2=document.querySelector("#pid")
+    let myvar3=document.querySelector("#part1")
+    let myvar4=document.querySelector("#part2")
+    let myvar5=document.querySelector("#full")
+    let myvar6=document.querySelector("#pop")
+    let myvar7=document.querySelector("#banner")
+    let myvar8=document.querySelector("#native")
+    let myvar9=document.querySelector("#skim")
+
+    myvar1.innerHTML="Pop_Under"
+    myvar2.innerHTML="First button is clicked"
+    myvar3.style.flex=4
+    myvar4.style.flex=8
+    myvar5.style.width="100%"
+    myvar6.style.color="white"
+    myvar6.style.backgroundColor=orange
+    
+    myvar7.style.color="black"
+    myvar8.style.color="black"
+    myvar9.style.color="black"
+}
+function mymessage2(){
+    let myvar1=document.querySelector("#htitle")
+    let myvar2=document.querySelector("#pid")
+    let myvar3=document.querySelector("#part1")
+    let myvar4=document.querySelector("#part2")
+    let myvar5=document.querySelector("#full")
+    let myvar6=document.querySelector("#pop")
+    let myvar7=document.querySelector("#banner")
+    let myvar8=document.querySelector("#native")
+    let myvar9=document.querySelector("#skim")
+
+    myvar1.innerHTML="BANNER AD"
+    myvar2.innerHTML="SECOND button is clicked"
+    myvar3.style.flex=4
+    myvar4.style.flex=8
+    myvar5.style.width="100%"
+    myvar6.style.color="black"
+    myvar7.style.color="white"
+    myvar7.style.backgroundColor=orange
+   
+    myvar8.style.color="black"
+    myvar9.style.color="black"
+}
+function mymessage3(){
+    let myvar1=document.querySelector("#htitle")
+    let myvar2=document.querySelector("#pid")
+    let myvar3=document.querySelector("#part1")
+    let myvar4=document.querySelector("#part2")
+    let myvar5=document.querySelector("#full")
+    let myvar6=document.querySelector("#pop")
+    let myvar7=document.querySelector("#banner")
+    let myvar8=document.querySelector("#native")
+    let myvar9=document.querySelector("#skim")
+
+    myvar1.innerHTML="NATIVE"
+    myvar2.innerHTML="THIRD button is clicked"
+    myvar3.style.flex=4
+    myvar4.style.flex=8
+    myvar5.style.width="100%"
+    myvar6.style.color="black"
+    myvar7.style.color="black"
+    myvar8.style.color="white"
+    myvar8.style.backgroundColor=orange
+    
+    myvar9.style.color="black"
+}
+function mymessage4(){
+    let myvar1=document.querySelector("#htitle")
+    let myvar2=document.querySelector("#pid")
+    let myvar3=document.querySelector("#part1")
+    let myvar4=document.querySelector("#part2")
+    let myvar5=document.querySelector("#full")
+    let myvar6=document.querySelector("#pop")
+    let myvar7=document.querySelector("#banner")
+    let myvar8=document.querySelector("#native")
+    let myvar9=document.querySelector("#skim")
+
+    myvar1.innerHTML="SKIM"
+    myvar2.innerHTML="FORTH button is clicked"
+    myvar3.style.flex=4
+    myvar4.style.flex=8
+    myvar5.style.width="100%"
+    myvar6.style.color="black"
+    myvar7.style.color="black"
+    myvar8.style.color="black"
+    myvar9.style.color="white"
+    myvar9.style.backgroundColor=orange
+    
+}
